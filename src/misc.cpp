8958ba45b9c12a4ce075f02d1b38a84f1d036944
rjdmoore <3844483+rjdmoore@users.noreply.github.com>
2018-08-17 00:00:44 +0200

    added funcs for setting process/thread priorities in windows

--- src/misc.cpp
+++ src/misc.cpp
@@ -8,6 +8,9 @@
 
 #include "timing.h"
 
+#define NOMINMAX    // stop min/max macros being defined in windows.h
+#include <windows.h>
+
 #include <cstdio>
 
 ///
@@ -21,4 +24,38 @@ int getchar_clean()
         ret = std::getchar();
     } while ((elapsed_secs() - t1) < 0.1);
     return ret;
+}
+
+///
+/// Set process to high priority.
+///
+bool SetProcessHighPriority()
+{
+    /// Sets process class to high priority level - care should be taken if FicTrac process uses all available CPU...
+    /// See https://docs.microsoft.com/en-us/windows/desktop/procthread/scheduling-priorities
+    return SetPriorityClass(GetCurrentProcess(), HIGH_PRIORITY_CLASS);
+}
+
+///
+/// Set process to high priority.
+///
+bool SetThreadVeryHighPriority()
+{
+    /// Sets thread priority to high.
+    /// See https://docs.microsoft.com/en-us/windows/desktop/procthread/scheduling-priorities
+    return SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_HIGHEST);
+}
+
+bool SetThreadHighPriority()
+{
+    /// Sets thread priority to high.
+    /// See https://docs.microsoft.com/en-us/windows/desktop/procthread/scheduling-priorities
+    return SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_ABOVE_NORMAL);
+}
+
+bool SetThreadNormalPriority()
+{
+    /// Sets thread priority to normal.
+    /// See https://docs.microsoft.com/en-us/windows/desktop/procthread/scheduling-priorities
+    return SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_NORMAL);
 }
